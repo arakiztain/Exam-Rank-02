633bf02b4221f59244cca54caa6efa373ef907de
arakiztain <arakiztain.gaizka@gmail.com>
2026-01-13 11:30:16 +0100

    finish

--- level2/print_bits.c
+++ level2/print_bits.c
@@ -6,7 +6,7 @@
 /*   By: garakizt <garakizt@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 12:24:30 by garakizt          #+#    #+#             */
-/*   Updated: 2025/12/30 13:05:21 by garakizt         ###   ########.fr       */
+/*   Updated: 2026/01/13 11:30:03 by garakizt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 void    print_bits(unsigned char octet)
 {
-    int i;
-    unsigned char bit;
-
-    i = 8;
-    bit = 0;
-    while (i--)
+    int i = 7;
+    char bit;
+    
+    while (i >= 0)
     {
-        bit = (octet >> i & 1) + 48;
+        if ((octet >> i) & 1)
+            bit = '1';
+        else
+            bit = '0';
         write(1, &bit, 1);
+        i--;
     }
 }
